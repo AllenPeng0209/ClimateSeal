2391e3609691b8280e9ecb718abda24be6f453b1
JinRW <jrw970326@gmail.com>
2025-05-07 12:54:37 +0800

    小功能迭代

--- app/components/workbench/CarbonFlow/carbonpanel.tsx
+++ app/components/workbench/CarbonFlow/carbonpanel.tsx
@@ -867,7 +867,7 @@ export function CarbonCalculatorPanel() {
                             pagination={{ pageSize: 5 }}
                             className="file-upload-table"
                         />
-                    </div>
+                    </div>  
                 </Card>
             </Col>
 
@@ -922,7 +922,7 @@ export function CarbonCalculatorPanel() {
                     <div className="mb-4 flex-shrink-0 filter-controls flex justify-between items-center">
                         <Space>
                             <Input placeholder="排放源名称" prefix={<SearchOutlined />} style={{width: 120}} />
-                            <Select placeholder="排放源类别" allowClear style={{width: 60}}>
+                            <Select placeholder="排放源类别" allowClear style={{width: 120}}>
                                 {emissionCategories.map(cat => <Select.Option key={cat} value={cat}>{cat}</Select.Option>)}
                             </Select>
                             <Button type="primary" icon={<SearchOutlined />}>查询</Button>
