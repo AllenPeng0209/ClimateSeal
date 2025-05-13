c5dd9e55ccb445edf5fd71462e3a0476564afc1d
fedavinci <xt806633519@gmail.com>
2025-05-13 16:50:45 +0800

    remove theme switch

--- app/components/sidebar/Menu.client.tsx
+++ app/components/sidebar/Menu.client.tsx
@@ -2,7 +2,7 @@ import { motion, type Variants } from 'framer-motion';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { Dialog, DialogButton, DialogDescription, DialogRoot, DialogTitle } from '~/components/ui/Dialog';
-import { ThemeSwitch } from '~/components/ui/ThemeSwitch';
+// import { ThemeSwitch } from '~/components/ui/ThemeSwitch';
 import { ControlPanel } from '~/components/@settings/core/ControlPanel';
 import { SettingsButton } from '~/components/ui/SettingsButton';
 import { db, deleteById, getAll, chatId, type ChatHistoryItem, useChatHistory } from '~/lib/persistence';
@@ -278,7 +278,7 @@ export const Menu = () => {
           </div>
           <div className="flex items-center justify-between border-t border-gray-200 dark:border-gray-800 px-4 py-3">
             <SettingsButton onClick={handleSettingsClick} />
-            <ThemeSwitch />
+            {/* <ThemeSwitch /> */}
           </div>
         </div>
       </motion.div>
--- app/components/ui/Slider.tsx
+++ app/components/ui/Slider.tsx
@@ -25,7 +25,7 @@ export const Slider = genericMemo(<T,>({ selected, options, setSelected }: Slide
 
   return (
     <div className="flex items-center flex-wrap shrink-0 gap-1 bg-bolt-elements-background-depth-1 overflow-hidden rounded-full p-1">
-      <SliderButton selected={isLeftSelected} setSelected={() => setSelected?.(options.left.value)}>
+      {/* <SliderButton selected={isLeftSelected} setSelected={() => setSelected?.(options.left.value)}>
         {options.left.text}
       </SliderButton>
 
@@ -33,7 +33,7 @@ export const Slider = genericMemo(<T,>({ selected, options, setSelected }: Slide
         <SliderButton selected={isMiddleSelected} setSelected={() => setSelected?.(options.middle!.value)}>
           {options.middle.text}
         </SliderButton>
-      )}
+      )} */}
 
       <SliderButton selected={isRightSelected} setSelected={() => setSelected?.(options.right.value)}>
         {options.right.text}
@@ -51,6 +51,7 @@ export const Slider = genericMemo(<T,>({ selected, options, setSelected }: Slide
             </SliderButton>
           );
         }
+
         return null;
       })}
     </div>
