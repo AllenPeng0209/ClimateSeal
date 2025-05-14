9bb77bbead6b86f5615c15fd23ad4c667065201f
fedavinci <xt806633519@gmail.com>
2025-05-14 20:01:40 +0800

    fix: remove analysis close button

--- app/components/workbench/Workbench.client.tsx
+++ app/components/workbench/Workbench.client.tsx
@@ -209,14 +209,14 @@ export const Workbench = memo(
                       </PanelHeaderButton>
                     </div>
                   )}
-                  <IconButton
+                  {/* <IconButton
                     icon="i-ph:x-circle"
                     className="-mr-1"
                     size="xl"
                     onClick={() => {
                       workbenchStore.showWorkbench.set(false);
                     }}
-                  />
+                  /> */}
                 </div>
                 <div className="relative flex-1 overflow-hidden">
                   <View initial={{ x: '0%' }} animate={{ x: selectedView === 'code' ? '0%' : '-100%' }}>
